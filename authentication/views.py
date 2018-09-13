4d140b7531c8961e46435a792f66309e0dfc04b2
rocher2d2 <rocher2d2@gmail.com>
2018-09-13 11:50:41 +0200

    maj views

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def acceuil(request):
 	return render(request, 'authentication/acceuil.html', {})
+	
+def login(request):
+	return render(request, 'authentication/login.html', {})
